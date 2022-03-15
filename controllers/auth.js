d7349cf79897cf3a67f8eeb0c1e3678adf935d29
mack-code0 <macaulayhilbert202@gmail.com>
2022-03-15 21:57:45 +0800

    auth stage 1

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,38 @@
 const { validationResult } = require("express-validator")
+const bcryptjs = require("bcryptjs")
 const User = require("../models/user")
 
-exports.postSignup = (req, res, next)=>{
+
+exports.login = (req, res, next) => {
+    const { email, password } = req.body
+    User.find({ email })
+        .then(user => {
+            if (!user) {
+                const error = new Error("User not found!")
+                error.statusCode = 404
+                throw error
+            }
+
+            return bcryptjs.compare(password, user.password)
+        })
+        .then(isMatch=>{
+            if(!isMatch){
+                const error = new Error("Wrong Password!")
+                error.statusCode = 401
+                throw error
+            }
+
+
+        })
+        .catch(err=>{
+            if (!err.statusCode) {
+                err.statusCode = 500
+            }
+            next(err)
+        })
+}
+
+exports.postSignup = (req, res, next) => {
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
         const error = new Error("Validation failed! Entered data is Incorrect.")
@@ -10,7 +41,23 @@ exports.postSignup = (req, res, next)=>{
         throw error
     }
 
-    const {email, name, password} = req.body
+    const { email, name, password } = req.body
 
-    
+    bcryptjs.hash(password, 12)
+        .then(hashedPassword => {
+            const newUser = new User({
+                email,
+                name,
+                password: hashedPassword
+            })
+            return newUser.save()
+        })
+        .then(user => {
+            res.status(201).json({ message: "New User created!", userId: user._id })
+        }).catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500
+            }
+            next(err)
+        })
 }
--- routes/auth.js
+++ routes/auth.js
@@ -1,21 +1,25 @@
 const express = require("express")
 const router = express.Router()
-const { check } = require("express-validator")
+const { body } = require("express-validator")
 const User = require("../models/user")
 const authController = require("../controllers/auth")
 
 
-router.post("/signup",
-    check("email").isEmail().custom((value, { req }) => {
-        User.findOne({email: value})
-        .then(user=>{
-            if(user){
-                return Promise.reject("Email address already exists")
-            }
-        })
-    }).normalizeEmail(),
-    check("password").trim().isLength({min: 5}),
-    check("name").trim().not().isEmpty(),
-    authController.postSignup)
+
+router.post("/login", authController.login)
+
+
+router.put("/signup", [
+    body("email").isEmail().withMessage("This is not an email").custom((value, { req }) => {
+        return User.findOne({ email: value })
+            .then(user => {
+                if (user) {
+                    return Promise.reject("Email address already exists")
+                }
+            })
+    }),
+    body("password").trim().isLength({ min: 5 }),
+    body("name").trim().not().isEmpty()
+], authController.postSignup)
 
 module.exports = router
